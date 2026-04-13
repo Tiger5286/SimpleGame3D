fa4a4fc55dcc77b8a6488a0484dd0aa4def41d3f
Tiger5286 <toratiger5286@gmail.com>
2026-04-13 18:11:49 +0900

    GameObjectクラス追加

---
+++ Project/Source/Game/GameObjects/GameObject.cpp
@@ -0,0 +1,59 @@
+﻿#include "GameObject.h"
+#include "../../Game.h"
+
+namespace
+{
+	constexpr float kGravity = 1.0f;
+	constexpr float kMaxFallSpeed = 10.0f;
+
+	constexpr float kResistance = 0.3f;
+}
+
+GameObject::GameObject(float sphereRadius):
+	m_sphere(sphereRadius)
+{
+}
+
+GameObject::~GameObject()
+{
+	MV1DeleteModel(m_modelHandle);
+}
+
+void GameObject::SetHandle(int handle)
+{
+	m_modelHandle = handle;
+}
+
+void GameObject::Gravity()
+{
+	m_vel.y -= kGravity;
+	if (m_vel.y < -kMaxFallSpeed)
+	{
+		m_vel.y = -kMaxFallSpeed;
+	}
+
+	m_pos += m_vel;
+	if (m_pos.y < 0.0f)
+	{
+		m_pos.y = 0.0f;
+		m_vel.y = 0.0f;
+	}
+}
+
+void GameObject::Resistance()
+{
+	// 速度と逆向きのベクトルを生成
+	Vector3 resist = -m_vel;
+	// Y方向には抵抗を適用しない
+	resist.y = 0.0f;
+	// 長さをkResistanceに補正
+	resist.Normalize();
+	resist *= kResistance;
+	// 速度に抵抗を加算し減速
+	m_vel += resist;
+}
+
+Vector3 GameObject::GetPos() const
+{
+	return m_pos;
+}
---
+++ Project/Source/Game/GameObjects/GameObject.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "../../Utility/Sphere.h"
+
+enum class ObjectTag
+{
+	None,
+	Player,
+	Enemy,
+	Chest,
+	Coin,
+	Punch,
+};
+
+class GameObject
+{
+public:
+	GameObject(float sphereRadius);
+	virtual ~GameObject();
+
+	virtual void Init() abstract;
+	virtual void End() abstract;
+	virtual void Update() abstract;
+	virtual void Draw() abstract;
+
+	// 他のオブジェクトと当たったときに呼ばれる関数
+	virtual void OnCollision(const GameObject& other) abstract;
+	// 他のオブジェクトと当たったときに呼ばれる関数(constでない版、危ないのは承知の上で使う)
+	virtual void OnCollision2(GameObject& other) {};
+
+	// Init()より前に実行する
+	void SetHandle(int handle);
+
+	Vector3 GetPos() const;
+
+	const Sphere& GetSphere() const { return m_sphere; }
+
+	ObjectTag GetTag() const { return m_tag; }
+
+	bool IsHit() const { return m_isHit; }
+	void OnHit() { m_isHit = true; }
+
+protected:
+	void Gravity();
+	void Resistance();
+
+protected:
+	Vector3 m_pos;
+	Vector3 m_vel;
+	int m_modelHandle = -1;
+
+	Sphere m_sphere;
+
+	ObjectTag m_tag = ObjectTag::None;
+
+	bool m_isHit = false;
+};
