e99afaf557adce397dfb04f3e27f0d17c5772b3a
Tiger5286 <toratiger5286@gmail.com>
2026-04-13 15:24:29 +0900

    Applicationクラス作成

---
+++ Project/Source/Application.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+class Application
+{
+public:
+	static Application& GetInstance();
+	virtual ~Application() = default;
+
+	Application(const Application&) = delete;
+	Application& operator=(const Application&) = delete;
+private:
+	Application() = default;
+
+public:
+	bool Init();
+	void Run();
+	void Terminate();
+
+	void RequestExit() { m_isRequestExit = true; }
+
+private:
+	bool m_isRequestExit = false;
+};
+
---
+++ Project/Source/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "DxLib.h"
+#include "Game.h"
+#include <memory>
+
+#include "Application.h"
+
+// プログラムは WinMain から始まる
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// アプリケーションのインスタンスを取得
+	auto& app = Application::GetInstance();
+	// アプリケーションの初期化
+	if (!app.Init())
+	{
+		return -1;			// 初期化に失敗したら終了
+	}
+	app.Run();				// メインループを開始
+	app.Terminate();		// 終了処理
+
+	return 0;				// ソフトの終了 
+}
